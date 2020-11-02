61ed441267015816183e3bcedca55742b0ce3173
aperinevg <aperinevg@yandex.ru>
2020-11-02 17:15:18 +0300

    Lesson 18, добавили страницу диалогов, без верстки

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,9 +3,9 @@ import MyPosts from './Posts/MyPosts/MyPosts';
 import classes from './Profile.module.css';
 
 const Profile = () => {
-     return (<div className={classes.content}>
+     return (<div>
         <div>
-            <img className={classes.contentFoto} src="https://focus.ua/storage/pub/images/2017/2615387.jpg" alt="" /></div>
+            <img className={classes.contentFoto} src="https://lh3.googleusercontent.com/proxy/e_Y4g1eHYQ0Ymxu06jPAOtpr8D3WuLxdGLHj7fPvHCOYYMIihkf0MXkyW0lcbJFED8GuNkmUcoOO8O9h5Z8FB3g9-bTB-M5OFtHwmW1Yy9Y4M8JsRhLrq54mtgEBv7Ae" alt="" /></div>
             <div>Avatar + description</div>
             <MyPosts/>        
         </div>);
